7728c090760dc92c788373d463d05b4d254782a9
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-05-14 10:24:58 +0200

    first commit

---
+++ README.rst
@@ -0,0 +1,24 @@
+Speciality Distribution
+=======================
+
+Speciality Distribution for TYPO3 CMS.
+
+Motivation
+----------
+
+All started with the modernisation of our Dummy package we were using in our company. To give a bit of background, we were aiming to:
+
+* Have Twitter Bootstrap as HTML / CSS Framework
+* Use as much as possible Fluid for the rendering and the templating. Actually, it turned out we have reached the 100% thanks to the work of Claus Due
+* Keep folder fileadmin clean from TS / JS / CSS files which should be for storing media only (images, documents etc…)
+
+We wanted not only a package to demonstrate the capability of TYPO3 but also something useful so that it should save us from the tedious
+and repeating work when kick-starting a website. The result is pretty much promising.
+More important we **have put everything in public** so that you can test and also take advantage for your own needs.
+
+
+How to install?
+===============
+
+- Install TYPO3, download the current stable Core http://get.typo3.org/current.
+- You will be guided until the list of distributions, just pick Speciality Distribution.
---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "introduction".
+ *
+ * Auto generated 03-05-2014 17:29
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Speciality Distribution',
+	'description' => 'This package delivers a special website just for you',
+	'category' => 'distribution',
+	'shy' => 0,
+	'version' => '0.1.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Fabien Udriot',
+	'author_email' => 'support@ecodev.ch',
+	'author_company' => '',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'typo3' => '6.2.0-6.2.99',
+			'speciality' => '',
+		),
+		'conflicts' =>
+		array (
+		),
+		'suggests' =>
+		array (
+		),
+	),
+);
+
+?>
