eef0e38a711e7c29c9765fbafd81cf7c524ab70c
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-04-01 20:42:51 +0200

    [TASK] Streamline ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,5 @@
 <?php
-
-/***************************************************************
- * Extension Manager/Repository config file for ext "introduction".
- *
- * Auto generated 03-05-2014 17:29
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'Speciality Distribution',
 	'description' => 'Delivers a special website just for you. This distribution can be installed via Composer with command ``composer create-project ecodev/typo3-cms-speciality-distribution htdocs dev-master``',
 	'category' => 'distribution',
@@ -20,25 +9,27 @@ $EM_CONF[$_EXTKEY] = array (
 	'author_company' => 'Ecodev',
 	'version' => '1.3.0-dev',
 	'constraints' =>
-	array (
+	[
 		'depends' =>
-		array (
+		[
 			'typo3' => '7.6.0-7.99.99',
 			'speciality' => '',
 			'scheduler' => '',
 			'realurl' => '',
 			'vidi' => '',
+			'vidi_frontend' => '',
+			'natural_gallery' => '',
 			'media' => '',
 			'seo_basics' => '',
 			'typo3_console' => '',
 			'nc_staticfilecache' => '',
-		),
+		],
 		'conflicts' =>
-		array (
+		[
 			'css_styled_content' => '',
-		),
+		],
 		'suggests' =>
-		array (
-		),
-	),
-);
+		[
+		],
+	],
+];
