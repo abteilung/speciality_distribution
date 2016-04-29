7158b5485662ba9b52b2796af36be45bc1349d5b
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-04-29 16:33:38 +0200

    [RELEASE] 1.2.10 - update SQL dump
    https://github.com/Ecodev/speciality_distribution/compare/1.2.9...1.2.10

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
 	'author' => 'Fabien Udriot',
 	'author_email' => 'support@ecodev.ch',
 	'author_company' => 'Ecodev',
-	'version' => '1.3.0-dev',
+	'version' => '1.2.10',
 	'constraints' =>
 	[
 		'depends' =>
