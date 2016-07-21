4729f0fbd4ddb4b794d916d38fee7a27a8226ace
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-07-21 09:07:09 +0200

    [TASK] Prepare next release

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
 	'author' => 'Fabien Udriot',
 	'author_email' => 'fabien@ecodev.ch',
 	'author_company' => 'Ecodev',
-	'version' => '1.2.10',
+	'version' => '1.3.0-dev',
 	'constraints' =>
 	[
 		'depends' =>
